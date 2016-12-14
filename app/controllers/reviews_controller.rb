6cfa0ab68e847086991bf51aa91940cd63820920
Ryo <kanadajinlee@yahoo.ca>
2016-12-14 01:16:31 +0000

    Users can now submit reviews on specific products

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,14 @@
 class ReviewsController < ApplicationController
 
   def create
-    @reviews = Review.all.order(created_at: :desc)
+    puts params
+    @review = Review.create(
+      description: params[:review][:description],
+      user: current_user,
+      rating:params[:review][:rating],
+      product_id:params[:product_id]
+    )
+    redirect_to product_path(params[:product_id]) #redirect back to same page
   end
 
   # def show
